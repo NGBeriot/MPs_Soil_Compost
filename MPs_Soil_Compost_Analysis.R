a55b497b31ea434b7153d0e64ac300da07de5bb7
Nicolas Beriot <65182851+NGBeriot@users.noreply.github.com>
2021-04-16 17:59:37 +0200

    Update MPs_Soil_Compost_Analysis.R

--- MPs_Soil_Compost_Analysis.R
+++ MPs_Soil_Compost_Analysis.R
@@ -11,12 +11,13 @@
   if(!require(plyr)){install.packages("plyr")}   
   if(!require(multcompView)){install.packages("multcompView")} 
   if(!require(pals)){install.packages("pals")}
+  if(!require(RColorBrewer)){install.packages("RColorBrewer")}
 
 #Loading data ####
   rm(list=ls()) # cleaning console
   graphics.off() # cleaning plots
   
-  wd="C:/Users/berio001/OneDrive - WageningenUR/PhD/Field Assessment/Benjamin/Submission" #C:/Users/Directory# Write the directory with SAS_results.xlsx and Raw_data_Beriot.xlsx
+  wd="C:/Users/berio001/OneDrive - WageningenUR/PhD/Field Assessment/Benjamin/Submission" #C:/Users/Directory# Write the directory wich contains SAS_results.xlsx and Raw_data_Beriot.xlsx
   setwd(wd)
 
  MPs <- read.csv("MPs_results.csv", sep=",")
@@ -45,7 +46,7 @@
 #Length Catagories####
   lenght_fraction=c(0.1, 0.2, 0.3, 0.5)
   lenght_fraction=data.frame( cat= c("M_n_Cat1", "M_n_Cat2", "M_n_Cat3" ,"M_n_Cat4", "M_n_Cat5"),
-                              cat_label=factor(c("< 100 µm", "100-200 µm", "200-300 µm", "300-500 µm", "> 500 µm")),
+                              cat_label=factor(c("< 100 Âµm", "100-200 Âµm", "200-300 Âµm", "300-500 Âµm", "> 500 Âµm")),
                               fraction=c(0.1, 0.2, 0.3, 0.5, 1) )
 
   # Check the number of MPs per category : 
@@ -56,11 +57,11 @@
       nrow(subset(MPs, MPs$LD>lenght_fraction$fraction[4])))
   
   # Assign name catogory
-    MPs$Areacatagory[MPs$LD< lenght_fraction$fraction[1]]="< 100 µm"
-    MPs$Areacatagory[MPs$LD< lenght_fraction$fraction[2]& MPs$LD> lenght_fraction$fraction[1]]="100-200 µm"
-    MPs$Areacatagory[MPs$LD< lenght_fraction$fraction[3]& MPs$LD> lenght_fraction$fraction[2]]="200-300 µm"
-    MPs$Areacatagory[MPs$LD< lenght_fraction$fraction[4]& MPs$LD> lenght_fraction$fraction[3]]="300-500 µm"
-    MPs$Areacatagory[MPs$LD> lenght_fraction$fraction[4]]="> 500 µm"
+    MPs$Areacatagory[MPs$LD< lenght_fraction$fraction[1]]="< 100 Âµm"
+    MPs$Areacatagory[MPs$LD< lenght_fraction$fraction[2]& MPs$LD> lenght_fraction$fraction[1]]="100-200 Âµm"
+    MPs$Areacatagory[MPs$LD< lenght_fraction$fraction[3]& MPs$LD> lenght_fraction$fraction[2]]="200-300 Âµm"
+    MPs$Areacatagory[MPs$LD< lenght_fraction$fraction[4]& MPs$LD> lenght_fraction$fraction[3]]="300-500 Âµm"
+    MPs$Areacatagory[MPs$LD> lenght_fraction$fraction[4]]="> 500 Âµm"
 
 
 # MPs nass estimation ####
@@ -84,7 +85,7 @@
 
   # *** Sphere ####
     # Zhang et al (2018) proposes to use the particles area to estimate the volume of the plastic particles as 
-    # the ninth of sphere of radius ???(Area). ). However Zhang et al (2018) measured the area of the particles after heating
+    # the ninth of sphere of radius âˆš(Area). ). However Zhang et al (2018) measured the area of the particles after heating
     #  assuming that the melting of the MPs will give it the spherical shape 1/9 of a sphere
       MPs$Mass_sphere = 4* Density * sqrt( (MPs$Area)^3/ pi) /27# [mg]
 
@@ -135,7 +136,7 @@
                                                                         "n_Cat1", "n_Cat2", "n_Cat3", "n_Cat4", "n_Cat5") ]*200
 
 # Summary Treat/layer ####
-# Average per treat 
+# Average per treat/Layer
   Treat_layer_summary_R=Sample_summary_R %>% 
     dplyr::group_by(Treat, Layer, Treat_Layer, Sample_type  ) %>%
     dplyr::summarise( n_samples=n(),M_n_particules=mean(n_particules),
@@ -150,10 +151,11 @@
                       SD_Mass_ellips=sd(Mass_ellips),  SD_Mass_sphere=sd(Mass_sphere),
                       SD_Mass_cube=sd(Mass_cube),
                       SD_n_Cat5= sd(n_Cat5),
-                      Q1=quantile(n_particules)[4]    )
+                      Q1_n=quantile(n_particules)[4],
+                      Q1_a=quantile(Mean_particle_Area)[4]  )
 
 # Summary Sample type ####
-# Average per treat 
+# Average per Sample_type 
   Type_summary_R=Sample_summary_R %>% 
     dplyr::group_by( Sample_type  ) %>%
     dplyr::summarise( n_samples=n(),M_n_particules=mean(n_particules),M_Tot_Area=mean(Tot_Area), 
@@ -164,7 +166,26 @@
                       SD_n_particules=sd(n_particules), SD_Tot_Area=sd(Tot_Area), 
                       SD_Mass_ellips=sd(Mass_ellips),  SD_Mass_sphere=sd(Mass_sphere),
                       SD_Mass_cube=sd(Mass_cube), 
-                      SD_n_Cat5= sd(n_Cat5))    
+                      SD_n_Cat5= sd(n_Cat5))  
+# Summary treat ####   
+    # Average per treat/Layer
+    Treat_summary_R=Sample_summary_R %>% 
+      dplyr::group_by(Treat, Sample_type  ) %>%
+      dplyr::summarise( n_samples=n(),M_n_particules=mean(n_particules),
+                        M_Tot_Area=mean(Tot_Area), 
+                        M_Mass_ellips=mean(Mass_ellips), M_Mass_sphere=mean(Mass_sphere),
+                        M_Mass_cube=mean(Mass_cube),
+                        M_Minor=mean(Minor), M_Major=mean(Major), 
+                        M_Mean_particle_Area=mean(Mean_particle_Area), 
+                        Q2_Mean_particle_Area=median(Mean_particle_Area), 
+                        M_n_Cat1= mean(n_Cat1), M_n_Cat2= mean(n_Cat2) ,  M_n_Cat3= mean(n_Cat3),
+                        M_n_Cat4= mean(n_Cat4), M_n_Cat5= mean(n_Cat5),
+                        SD_n_particules=sd(n_particules), SD_Tot_Area=sd(Tot_Area), 
+                        SD_Mass_ellips=sd(Mass_ellips),  SD_Mass_sphere=sd(Mass_sphere),
+                        SD_Mass_cube=sd(Mass_cube),
+                        SD_n_Cat5= sd(n_Cat5),
+                        Q1_n=quantile(n_particules)[4],
+                        Q1_a=quantile(Mean_particle_Area)[4]  )
 
 # Statistical analysis #### 
 
@@ -228,7 +249,7 @@
                              Letters=letters,
                              reversed = FALSE)
 
-  # ***Size distribution ####
+  # *** mm2 Size distribution ####
     # Kolmogorov-Smirnov test, for loop
         # Matrix of p-values : p_KS
           p_KS=matrix(0,nrow(Treat_layer_summary_R),nrow(Treat_layer_summary_R))
@@ -247,7 +268,26 @@
                                    threshold=0.05,
                                    Letters=letters,
                                    reversed = FALSE)
-
+          
+    # *** um Size distribution ####
+          # Kolmogorov-Smirnov test, for loop
+          # Matrix of p-values : p_KS
+          p_KS_LD=matrix(0,nrow(Treat_layer_summary_R),nrow(Treat_layer_summary_R))
+          for (i in 1:nrow(Treat_layer_summary_R)){
+            for (j in 1:nrow(Treat_layer_summary_R)){
+              p_KS_LD[i,j]= ks.test(MPs$LD[MPs$Treat_Layer == Treat_layer_summary_R$Treat_Layer[i]], 
+                                 MPs$LD[MPs$Treat_Layer == Treat_layer_summary_R$Treat_Layer[j]])[[2]]
+            }
+          }
+          colnames(p_KS_LD)=Treat_layer_summary_R$Treat_Layer
+          row.names(p_KS_LD)=Treat_layer_summary_R$Treat_Layer
+          
+          # Assign different letters to significant differences  
+          mcl_MPLDd=multcompLetters(p_KS_LD,
+                                   compare="<=",
+                                   threshold=0.05,
+                                   Letters=letters,
+                                   reversed = FALSE)
 #*** Mean Area ####
   kruskal.test(Area ~ Treat_Layer, data = MPs) #Kruskal-Wallis chi-squared = 3363.1, df = 11, p-value < 2.2e-16
     
@@ -268,7 +308,7 @@
 # Plots ####
 
   #*** Graphical Abstract Size distribution, MPs####
-    MPs$Areacatagory=factor(MPs$Areacatagory, levels=c("< 100 µm", "100-200 µm", "200-300 µm", "300-500 µm", "> 500 µm"))
+    MPs$Areacatagory=factor(MPs$Areacatagory, levels=c("< 100 Âµm", "100-200 Âµm", "200-300 Âµm", "300-500 Âµm", "> 500 Âµm"))
     
     # data.frame PLOT1 : P_distrib
       P_distrib=rbind(Type_summary_R, Type_summary_R,Type_summary_R,Type_summary_R,Type_summary_R)
@@ -282,10 +322,10 @@
         P_distrib$n_cat[ ((i-1)*nrow(Type_summary_R)+1): (i*nrow(Type_summary_R)) ]= t(as.vector(Type_summary_R[,lenght_fraction$cat[i]]))
       }
       
-      P_distrib$length_cat=revalue(factor(P_distrib$length_cat), c("5"="> 500 µm", "4"="300-500 µm",  "3"="200-300 µm", "2"="100-200 µm", "1"="< 100 µm"   ))
-      P_distrib$length_cat=revalue(factor(P_distrib$length_cat), c( "1"="< 100 µm" , "2"="100-200 µm",  "3"="200-300 µm","4"="300-500 µm", "5"="> 500 µm"  ))
+      P_distrib$length_cat=revalue(factor(P_distrib$length_cat), c("5"="> 500 Âµm", "4"="300-500 Âµm",  "3"="200-300 Âµm", "2"="100-200 Âµm", "1"="< 100 Âµm"   ))
+      P_distrib$length_cat=revalue(factor(P_distrib$length_cat), c( "1"="< 100 Âµm" , "2"="100-200 Âµm",  "3"="200-300 Âµm","4"="300-500 Âµm", "5"="> 500 Âµm"  ))
       
-      P_distrib$length_cat=factor(P_distrib$length_cat, levels=rev(c("< 100 µm", "100-200 µm", "200-300 µm", "300-500 µm", "> 500 µm") ))
+      P_distrib$length_cat=factor(P_distrib$length_cat, levels=rev(c("< 100 Âµm", "100-200 Âµm", "200-300 Âµm", "300-500 Âµm", "> 500 Âµm") ))
       P_distrib$Sample_type=factor(P_distrib$Sample_type,c("Soil_Sp","Soil_NL" ,"Compost_NL"))
     
     # color patette PLOT1
@@ -328,6 +368,9 @@
     # data.frame PLOT2 : Sample_summary_R
     
     # Letters significant differences for PLOT3
+    Sample_summary_R$Treat_Layer=factor(Sample_summary_R$Treat_Layer)
+    Sample_summary_R= Sample_summary_R[order( Sample_summary_R$Treat_Layer),]
+    
       kruskal.test(n_particules ~ Treat_Layer, data =Sample_summary_R) #Kruskal-Wallis chi-squared = 103.44, df = 11, p-value < 2.2e-16
       cm_MP=compare_means(n_particules ~ Treat_Layer, data =Sample_summary_R, method = "wilcox.test",  # or method = "t.test"
                           paired = FALSE, group.by = NULL, ref.group = NULL)
@@ -338,6 +381,7 @@
                              threshold=0.05,
                              Letters=letters,
                              reversed = FALSE)
+      
       Treat_layer_summary_R=Treat_layer_summary_R[order(Treat_layer_summary_R$Treat_Layer),]
       Treat_layer_summary_R$Sign.letter=mcl_MP[[1]]
       
@@ -365,7 +409,7 @@
       stat_summary(data =Sample_summary_R, aes(x=Treat_Layer, y=n_particules, fill=Treat_Layer),
                    fun=mean, colour="black", geom="point", 
                    shape="+", size=7,  show.legend = FALSE)+ #pch = 24, cex=5, lwd=50,
-      geom_text(data = Treat_layer_summary_R ,aes(x=Treat_Layer, y= Q1+150, label=Sign.letter),
+      geom_text(data = Treat_layer_summary_R ,aes(x=Treat_Layer, y= Q1_n+150, label=Sign.letter),
                 position=position_nudge(x=-0.3, y=0), size=5, colour="black")+
       scale_x_discrete(name= NULL, breaks=c("Sp (0-10)","Sp (10-30)" ,"NL1 (0-10)","NL1 (10-30)","NL2 (0-10)", "NL2 (10-30)", "Cm", "Cg"),
                        labels=c("Sp\n(0-10)","Sp\n(10-30)" ,"NL1\n(0-10)","NL1\n(10-30)","NL2\n(0-10)", "NL2\n(10-30)", "Cm", "Cg")  )+
@@ -381,12 +425,76 @@
         legend.position="none" ) 
     # Export 900*350
 
+    #*** Fig.3 Microplastic mean Area ####
+    # data.frame PLOT2 : Sample_summary_R
+    
+    # Letters significant differences for PLOT3
+    MPs$Treat_Layer=factor(MPs$Treat_Layer, c(  "Sp_0_10","Sp_10_30","NL1_0_10","NL1_10_30","NL2_0_10","NL2_10_30","Cm_0","Cg_0" ))
 
-# *** Fig.S1 Density size distribution ####
+    
+    kruskal.test(Area~ Treat_Layer, data =MPs) #Kruskal-Wallis chi-squared = 103.44, df = 11, p-value < 2.2e-16
+    cm_MPpa=compare_means(Area~ Treat_Layer, data =MPs, method = "wilcox.test",  # or method = "t.test"
+                        paired = FALSE, group.by = NULL, ref.group = NULL)
+    pval_MPpa=cm_MPpa$p # Create a p-value vector ...
+    names(pval_MPpa)<- paste(cm_MPpa$group1,cm_MPpa$group2,sep = "-") #...with hyphenated names to fit multcompLetters
+    mcl_MPpa=multcompLetters(pval_MPpa,
+                           compare="<=",
+                           threshold=0.05,
+                           Letters=letters,
+                           reversed = FALSE)
+    
+     Treat_layer_summary_R$Treat_Layer= factor(Treat_layer_summary_R$Treat_Layer,c("Sp (0-10)","Sp (10-30)","NL1 (0-10)","NL1 (10-30)","NL2 (0-10)","NL2 (10-30)","Cm","Cg"))
+     Treat_layer_summary_R=Treat_layer_summary_R[order(Treat_layer_summary_R$Treat_Layer),]
+     Treat_layer_summary_R$Sign.letter=mcl_MPpa[[1]]
+     
+     Treat_layer_summary_R$Sign.letter=mcl_MPAd[[1]] #Kolomogorov S
+    
+    
+   MPs$Treat_Layer=revalue(factor(MPs$Treat_Layer), c( "Sp_0_10"="Sp (0-10)", "Sp_10_30"="Sp (10-30)",
+                                                                                  "NL1_0_10"  ="NL1 (0-10)", "NL1_10_30"="NL1 (10-30)",
+                                                                                  "NL2_0_10"="NL2 (0-10)", "NL2_10_30"="NL2 (10-30)",
+                                                                                  "Cg_0"="Cg","Cm_0"="Cm") )
+  
+    Treat_layer_summary_R$Treat_Layer=revalue(factor(Treat_layer_summary_R$Treat_Layer), c( "Sp_0_10"="Sp (0-10)", "Sp_10_30"="Sp (10-30)",
+                                                                                           "NL1_0_10"  ="NL1 (0-10)", "NL1_10_30"="NL1 (10-30)",
+                                                                                           "NL2_0_10"="NL2 (0-10)", "NL2_10_30"="NL2 (10-30)",
+                                                                                           "Cg_0"="Cg","Cm_0"="Cm") )
+   
+   
+   MPs$Treat_Layer=factor(MPs$Treat_Layer,c("Sp (0-10)","Sp (10-30)" ,"NL1 (0-10)","NL1 (10-30)","NL2 (0-10)", "NL2 (10-30)", "Cm", "Cg"))
+
+   
+    # color patette PLOT
+    my_palette = c(brewer.pal(8, "Paired")[c(5,6,3,4,1,2,8,7)])
+    PLOT3= ggplot()+
+      geom_violin(data =MPs[MPs$Area<0.5,], aes(x=Treat_Layer, y=Area, fill=Treat_Layer), outlier.shape = -1)+
+      scale_fill_manual(values = my_palette )+
+      stat_summary(data = MPs[MPs$Area<0.5,], aes(x=Treat_Layer, y=Area, fill=Treat_Layer),
+                   fun=mean, colour="black", geom="point", 
+                   shape="+", size=7,  show.legend = FALSE)+ #pch = 24, cex=5, lwd=50,
+      geom_text(data = Treat_layer_summary_R ,aes(x=Treat_Layer, y= Q1_a+0.1, label=Sign.letter),
+                position=position_nudge(x=-0.3, y=0), size=5, colour="black")+
+      scale_x_discrete(name= NULL, breaks=c("Sp (0-10)","Sp (10-30)" ,"NL1 (0-10)","NL1 (10-30)","NL2 (0-10)", "NL2 (10-30)", "Cm", "Cg"),
+                       labels=c("Sp\n(0-10)","Sp\n(10-30)" ,"NL1\n(0-10)","NL1\n(10-30)","NL2\n(0-10)", "NL2\n(10-30)", "Cm", "Cg")  )+
+      ylab(expression("MPs area [mm"^2 *"]"))+
+      theme(
+        plot.title = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_text(size=16,hjust = 0.5),
+        axis.text.x  = element_text(size=14), #hjust : align the labels 
+        axis.text.y  = element_text(size=14),
+        legend.title = element_blank(),
+        legend.text = element_blank(),
+        legend.position="none" ) 
+    # Export 900*350
+    
+    
+# *** Density size distribution ####
   
   # data.frame Fig.S1 : P_density
     # Crop particles > 0.6 mm to zoom in the graphs
     P_density=MPs[MPs$Area<0.6,]
+    P_density=MPs[MPs$LD<1,]
     
     P_density$Treat_Layer=revalue(factor(P_density$Treat_Layer), c( "Sp_0_10"="Sp (0-10)", "Sp_10_30"="Sp (10-30)",
                                                                     "NL1_0_10"  ="NL1 (0-10)", "NL1_10_30"="NL1 (10-30)",
@@ -399,11 +507,11 @@
      
     my_palette =c( "#E31A1C", "black","#33A02C", "black", "#1F78B4",  "black", "#FF7F00","#FDBF6F")
     
-    PLOT3=ggplot( P_density, aes(x=Area, fill=Treat_Layer)) + 
+    PLOT4=ggplot( P_density, aes(x=LD*1000, fill=Treat_Layer)) + 
       geom_density(alpha=.3)+
       facet_wrap(~Treat) +
       scale_fill_manual(values = my_palette )+
-      xlab(expression("Particles area [mm"^2 *"]"))+
+      xlab(expression("Particles area [Âµm]"))+
       ylab("Density [%]")+
       theme(
         plot.title = element_blank(),
